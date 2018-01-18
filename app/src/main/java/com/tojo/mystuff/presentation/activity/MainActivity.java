25b4656c99ab2fa4d647080a6e8393d6b990f38e
TojoDev <admin@ETE-MC.local>
2018-01-18 11:59:00 +0300

    init project

---
+++ app/src/main/java/com/tojo/mystuff/presentation/activity/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tojo.mystuff.presentation.activity;
+
+import android.support.v4.app.FragmentActivity;
+
+import org.androidannotations.annotations.EActivity;
+
+/**
+ * Created by admin on 16/01/2018.
+ */
+
+@EActivity
+public class MainActivity extends FragmentActivity {
+
+}
